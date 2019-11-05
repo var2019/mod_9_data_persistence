34eb10fe1a18369455ce1ca9e4e76d7f8cbaa085
varsha <varsharedymandadi@gmail.com>
2019-11-05 12:40:59 -0700

    Questions 1-3 answered

--- data_persistence.py
+++ data_persistence.py
@@ -21,22 +21,31 @@ curs.execute(
      )
 '''
 #Answer the following Questions
-**1) Which database will you be using for your final project? (e.g.SQL Server, MongoDB, Other...) (5pts)
+1) Which database will you be using for your final project? (e.g.SQL Server, MongoDB, Other...) (5pts)
+    
+ We will be using SQL server for our final project
 
 
+2) Where will the database you are using be hosted? (e.g.Azure, Locally, Shared MIS Dept.) (5pts)
 
-** 2) Where will the database you are using be hosted? (e.g.Azure, Locally, Shared MIS Dept.) (5pts)
+ The database will be hosted in azure
+ 
 
+3) Why did you choose the database and hosting option you did? (10 pts)
 
+* We chose SQL server because we are pretty comfortable using it since we are also using it in our database management class
+* We chose Microsoft Azure since it provides various cloud services like analytics, storage and networking.
+* It provides simple and reliable data storage on huge scale
+* On top of these azure student account that we recently set up includes free access to azure products for 12 months and 
+$200 credit to spend on azure services for the first 30 days of sign up..!!
 
-** 3) Why did you choose the database and hosting option you did? (10 pts)
 
 
 ** Do the Following:
 
 ** 1) Setup the data base you will be using
 
-** 2) Connect to the datbase with a python script and upload the script.
+** 2) Connect to the database with a python script and upload the script.
 *If the script contains sensitive information such as credentials that cannot be shared,
 **please take a screen-shot of the connection and blot out sensitive information.(10 pts.)
 

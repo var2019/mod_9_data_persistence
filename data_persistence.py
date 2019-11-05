e1e5489c34f813a6338362251ded0f54ed6909b5
varsha <varsharedymandadi@gmail.com>
2019-11-05 12:14:21 -0700

    Fixing comments

--- data_persistence.py
+++ data_persistence.py
@@ -19,3 +19,31 @@ curs.execute(
     '''
 
      )
+'''
+#Answer the following Questions
+**1) Which database will you be using for your final project? (e.g.SQL Server, MongoDB, Other...) (5pts)
+
+
+
+** 2) Where will the database you are using be hosted? (e.g.Azure, Locally, Shared MIS Dept.) (5pts)
+
+
+
+** 3) Why did you choose the database and hosting option you did? (10 pts)
+
+
+** Do the Following:
+
+** 1) Setup the data base you will be using
+
+** 2) Connect to the datbase with a python script and upload the script.
+*If the script contains sensitive information such as credentials that cannot be shared,
+**please take a screen-shot of the connection and blot out sensitive information.(10 pts.)
+
+
+*3) Describe the schema you will use in your database.If it is a relational database (e.g.SQL Server),
+*share your table schema, if it is a document store (e.g.MongoDB) then share a sample JSON file that represents the data you intend to store.(10 pts)
+'''
+
+
+

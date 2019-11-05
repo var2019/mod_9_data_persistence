d3ca58bcb2a37e4e9d6060a92b0e1551e24d20b8
KSo <kristenmellor@yahoo.com>
2019-11-04 22:48:58 -0700

    pyodbc and execute code

--- data_persistence.py
+++ data_persistence.py
@@ -5,3 +5,17 @@ password = ""
 dbname = ""
 servername = ""
 connection_string = 'Driver={ODBC Driver 17 for SQL Server};Server=' + servername + ',1433;Database=' + dbname + ';Uid=' + username + ';Pwd=' + password + ';'
+
+conn = pyodbc.connect(connection_string,autocommit=True)
+curs = conn.cursor()
+
+curs.execute(
+    '''
+     create table someDataSet(
+     ID int primary key clustered identity(1,1)
+     , attribute1 datatype
+     , attribute2 datatype 
+     )
+    '''
+
+     )

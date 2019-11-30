2ebe0369e59615b4deb0835920388f80a942e483
tigur <thetigur@gmail.com>
2019-11-29 17:00:28 -0700

    Everything playing nice Endpoints for income and expenses

--- flask_api.py
+++ flask_api.py
@@ -1,41 +1,64 @@
-from sqlalchemy import create_engine
-from flask import Flask, g, render_template, abort, request
-import os
-import socket
+import sqlalchemy as db
+from flask import Flask, abort
+import json
+import decimal
+import datetime
 
-username = "kristinsohm"
+username = "kristensohm"
 password = ""
 dbname = "MIS5400"
 servername = "gidferd.database.windows.net"
-eng = create_engine('mssql+pyodbc://kristensohm:Gidferd7*@gidferd.database.windows.net:1433/MIS5400?Driver={ODBC Driver 17 for SQL Server}', echo=True)
+driver = "{ODBC Driver 17 for SQL Server}"
+engine = db.create_engine(f'mssql+pyodbc://{username}:{password}@{servername}:1433/{dbname}?Driver={driver}', echo=True)
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+# Establish a database connection before requests
 @app.before_request
 def before_request():
     try:
-        eng.connect()
+        engine.connect()
     except ConnectionError:
         abort("No database connection could be established.", 503)
 
+# Try really hard to dispose of the database connection cleanly
 @app.teardown_request
 def teardown_request(exception):
     try:
-        g.rdb_conn.close()
+        engine.dispose()
     except AttributeError:
         pass
+    finally:
+        engine.dispose()
 
-
-@app.route("/")
+# Default route with no info
+@app.route("/", methods=['GET'])
 def hello():
     html = "<h3>There be data here</h3>"
     return html.format()
 
-@app.route("/getdata")
-def get():
-    eng.connect()
-    return
+# Gets the data from the income table
+@app.route("/api/v1/income", methods=['GET'])
+def get_income():
+    query = "SELECT * FROM PersonalIncome"
+    results = engine.execute(query)
+    return json.dumps([dict(r) for r in results], default=alchemyencoder)
 
+# Gets the data from the consumption (expenses) table
+@app.route("/api/v1/expenses", methods=['GET'])
+def get_expenses():
+    query = "SELECT * FROM ['Personal Consumption']"
+    results = engine.execute(query)
+    return json.dumps([dict(r) for r in results], default=alchemyencoder)
+
+# Used to format decimals and dates properly on conversion from sqlalchemy object to json
+def alchemyencoder(obj):
+    if isinstance(obj, datetime.date):
+        return obj.isoformat()
+    elif isinstance(obj, decimal.Decimal):
+        return float(obj)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+
